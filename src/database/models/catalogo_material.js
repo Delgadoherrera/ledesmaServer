cf4895d9103e8fc4052225ffa499a9936ee8120c
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-10-10 18:58:26 -0300

    someFix

--- src/database/models/catalogo_material.js
+++ src/database/models/catalogo_material.js
@@ -1,7 +1,7 @@
 module.exports = (sequelize, dataTypes) => {
   let alias = "Catalogo_material";
   let cols = {
-    idCompra: {
+    id: {
       type: dataTypes.INTEGER,
       primaryKey: true,
       autoIncrement: true,
