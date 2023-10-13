9cce7eb5909f1091c331de2a649fb9d341a447b0
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-10-13 20:21:44 -0300

    seque

--- src/database/models/catalogo_material.js
+++ src/database/models/catalogo_material.js
@@ -19,7 +19,10 @@ module.exports = (sequelize, dataTypes) => {
     timestamps: false,
   };
   const Catalogo_material = sequelize.define(alias, cols, config);
-
+  Catalogo_material.hasOne(Catalogo_unidad_medida, {
+    foreignKey: "unidadMedida", // Nombre de la columna que relaciona los modelos
+    as: "unidadMedida", // Alias para la relación
+  });
 
   return Catalogo_material;
 };
