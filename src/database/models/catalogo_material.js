09f90efaaf1a6f2613a981a61aa8d133f5261d9d
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-10-14 17:44:49 -0300

    estado column

--- src/database/models/catalogo_material.js
+++ src/database/models/catalogo_material.js
@@ -23,10 +23,10 @@ module.exports = (sequelize, dataTypes) => {
   };
   const Catalogo_material = sequelize.define(alias, cols, config);
   Catalogo_material.associate = function (models) {
-    Catalogo_material.belongsTo(models.Catalogo_unidad_medida, {
+/*     Catalogo_material.belongsTo(models.Catalogo_unidad_medida, {
       foreignKey: "id", // Utiliza el campo correcto para la relación
       as: "unidadMedida", // Usa el alias correcto
-    });
+    }); */
     Catalogo_material.belongsTo(models.Catalogo_unidad_medida, {
       foreignKey: "unidadMedidaId", // Utiliza el campo correcto para la relación (unidadMedidaId)
       as: "unidadMedida", // Usa el alias correcto
