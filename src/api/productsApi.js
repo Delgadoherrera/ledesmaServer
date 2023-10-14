f91d5416d6eb4bf94fefc6d257d6d802891b2cb5
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-10-14 04:47:27 -0300

    estado column

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -115,6 +115,8 @@ router.post("/materiales/borrarMaterial/:id", (req, res) => {
 });
 
 router.post("/materiales/editar/:id", async (req, res) => {
+
+  console.log('EDITAR MATERIAL ID:', req.params.id)
   console.log("req.body", req.body);
   try {
     let unidadMedidaExistente = await Catalogo_unidad_medida.findOne({
