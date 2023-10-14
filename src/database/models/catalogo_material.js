9f0c1428e2df1c795222f92c6b233c4448d831ee
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-10-13 21:20:52 -0300

    ia model

--- src/database/models/catalogo_material.js
+++ src/database/models/catalogo_material.js
@@ -21,8 +21,8 @@ module.exports = (sequelize, dataTypes) => {
   const Catalogo_material = sequelize.define(alias, cols, config);
   Catalogo_material.associate = function (models) {
     Catalogo_material.belongsTo(models.Catalogo_unidad_medida, {
-      as: "catalogo_unidad_medida",
-      foreignKey: "id",
+      as: "Catalogo_unidad_medida", // Usa el mismo alias definido en el modelo de unidad de medida
+      foreignKey: "unidadMedida", // Usa el mismo campo que establece la relación
     });
   };
   return Catalogo_material;
