1d45d15ecd973ade6a833ba9f406be0a16a56c9f
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-10-20 21:32:11 -0300

    listarcosto

--- src/database/models/catalogo_costo.js
+++ src/database/models/catalogo_costo.js
@@ -20,7 +20,7 @@ module.exports = (sequelize, dataTypes) => {
   const Catalogo_costo = sequelize.define(alias, cols, config);
   Catalogo_costo.associate = function (models) {
     Catalogo_costo.belongsTo(models.Costo_item, {
-      foreignKey: "idCosto", // Utiliza el campo correcto para la relación
+      foreignKey: "id", // Utiliza el campo correcto para la relación
       as: "idCosto", // Usa el alias correcto
     });
   };
