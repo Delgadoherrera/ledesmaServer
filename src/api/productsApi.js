03cc7e0c4c6cdf2e99a7bb04889a3eb03ba63a3f
DelgadoHerrera <delgado.luciano.herrera@gmail.com>
2023-10-22 03:32:19 -0300

    api

--- src/api/productsApi.js
+++ src/api/productsApi.js
@@ -77,23 +77,39 @@ router.post("/materiales/nuevoMaterial", async (req, res) => {
 });
 router.post("/categorias/nuevaCategoriaProducto", async (req, res) => {
   console.log("req.body", req.body);
-  res
-    .status(201)
-    .json({ message: "Costo creado con éxito", material: 'nuevoCosto' });
-  /* try {
-    const { costo, concepto } = req.body;
-    const nuevoCosto = await Catalogo_costos.create({
-      costo: costo,
-      concepto: concepto,
-      estado:"activo"
+
+  try {
+    let categoriaExistente = await Catalogo_categoria_productos.findOne({
+      where: {
+        detalle: req.body.detalle,
+      },
     });
+
+    if (!categoriaExistente) {
+      categoriaExistente = await Catalogo_categoria_productos.create({
+        detalle: req.body.detalle,
+      });
+    }
+
+    /*   let item_categoria_existente = await Categoria_productos_item.findOne({
+    where: {
+      descripcion: req.body.descripcion,
+    },
+  });
+
+  if (!item_categoria_existente) {
+    item_categoria_existente = await Categoria_productos_item.create({
+      descripcion: req.body.descripcion,
+    });
+  } */
+
     res
       .status(201)
-      .json({ message: "Costo creado con éxito", material: nuevoCosto });
+      .json({ message: "Costo creado con éxito", material: "nuevoCosto" });
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Hubo un error al crear el material" });
-  } */
+  }
 });
 router.post("/materiales/borrarMaterial/:id", (req, res) => {
   Catalogo_materiales.update(
